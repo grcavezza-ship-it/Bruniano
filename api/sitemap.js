b40bb081b4239f3cfafa4c6eaac53cf38e955a48
grcavezza-ship-it <grcavezza@gmail.com>
2026-09-14 12:45:34 +0200

    chore: align sitemap URLs with www canonical domain

--- api/sitemap.js
+++ api/sitemap.js
@@ -1,6 +1,6 @@
 import { db } from './_db.js';
 
-const SITE_URL = 'https://centromedicobruniano.it';
+const SITE_URL = 'https://www.centromedicobruniano.it';
 const STATIC_PATHS = [
   '/',
   '/trattamenti.html',
